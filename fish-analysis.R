e9ae56ce8396a1e79d9c5eb014ea3bdf2d8344f8
Christopher Chizinski <chris.chizinski@gmail.com>
2018-10-23 14:03:23 -0500

    plot modification

--- fish-analysis.R
+++ fish-analysis.R
@@ -2,7 +2,7 @@ library(tidyverse)
 source("R/themes.R")
 
 #load data
-fish <- read_csv("D:/NRES800/gittutorial/data/Gaeta_etal_CLC_data.csv")
+fish <- read_csv("data/Gaeta_etal_CLC_data.csv")
 head(fish)
 
 #create new column in data called length_cat. length > 200 is big, otherwise small
@@ -21,3 +21,14 @@ fish %>%
   geom_histogram(aes(scalelength, fill = length_cat)) +
   facet_wrap(~length_cat, nrow = 1) +
   theme_pub()
+
+
+## Chris's modifications
+
+ggplot() +
+  geom_histogram(data = fish, aes(x = scalelength, fill = length_cat), colour = "black", bins = 25) +
+  facet_wrap(~length_cat) +
+  coord_cartesian(ylim = c(0,500), xlim = c(0, 12), expand = FALSE) +
+  theme_classic() +
+  theme(legend.position = "none",
+        panel.spacing = unit(2, "lines"))
